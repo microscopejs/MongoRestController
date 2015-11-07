9f04883e717b4344ed99d3977f6d15a350c951c9
bhtz <heintz.benjamin@gmail.com>
2015-11-07 22:15:44 +0100

    decorator

--- samples/controllers/ArticleController.js
+++ samples/controllers/ArticleController.js
@@ -1,10 +1,11 @@
 import MongoRestController from '../../src/mongoRestController';
 import {Model} from '../../src/decorators';
+import {decorators} from 'microscope-web';
 import Article from '../models/Article';
+var {url} = decorators;
 
+@url('/api/articles')
 @Model(Article)
 export default class ArticleController extends MongoRestController {
-	get baseUrl(){
-		return '/api/articles'
-	}
+
 }
